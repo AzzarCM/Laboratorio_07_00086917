8a70a933db6a0c2e7e8b1635ed470ae4a2acebce
Cristian <DARUBI@github.com>
2018-10-12 08:54:43 -0600

    asas

--- asset/js/elementId.js
+++ asset/js/elementId.js
@@ -25,6 +25,7 @@ container.innerHTML = "<input type=”text” placeholder=’escriba su texto’
 
 var containers = document.querySelectorAll(".c2"); //con getElementByClassName los elementos no son iterables con foreach y tendria que usar el for normalito.
 
+// almacena una lista ...  NodeList(6) [div.container.c2, div.container.c2, div.container.c2, div.container.c2, div.container.c2, div.container.c2]
 
 /*
 for(let i = 0; i<= containers.length; i++){
@@ -38,9 +39,17 @@ containers.forEach(element => {
     element.style.backgroundColor = "#009999";
 });
 
+// se muestran los divs con un color verdoso
+
+
+
 containers.forEach(x =>{
     x.innerHTML = "<input type='text' placeholder='Escriba su texto'>";
 });
+// se muestra los input en todos los container seleccionados con la clase c2
+// la diferencia es que innerText obtiene el contenido del texto de un nodo mientras que innerHTML obtiene la etiqueta en si.
+
+
 
 // eventos
 
@@ -59,3 +68,5 @@ btnCopy.onclick = function(evt)
 let msj = document.getElementById('textMsj').value;
 document.getElementById('showMsj').innerText = msj;
 }
+//lo que suscede es que el texyo que intruducimos en la caja de texto se copia al otro div que esta en color aqua   
+//value es para obtener el valor de una caja de texto mientras que innerText metemos texto.
